765b1b3ad4c394ec1bb3db8c9bde65ff5d07d62d
Hao Song <songhao1119@gmail.com>
2018-03-22 15:45:50 -0400

    Add Swagger and AWS S3

---
+++ src/main/java/com/picfood/server/controller/BucketController.java
@@ -0,0 +1,31 @@
+package com.picfood.server.controller;
+
+import com.picfood.server.service.AmazonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Created by shawn on 2018/3/22.
+ */
+@RestController
+@RequestMapping("/storage/")
+public class BucketController {
+
+    private AmazonClient amazonClient;
+
+    @Autowired
+    BucketController(AmazonClient amazonClient) {
+        this.amazonClient = amazonClient;
+    }
+
+    @PostMapping("/uploadFile")
+    public String uploadFile(@RequestPart(value = "file") MultipartFile file) {
+        return this.amazonClient.uploadFile(file);
+    }
+
+    @DeleteMapping("/deleteFile")
+    public String deleteFile(@RequestPart(value = "url") String fileUrl) {
+        return this.amazonClient.deleteFileFromS3Bucket(fileUrl);
+    }
+}
