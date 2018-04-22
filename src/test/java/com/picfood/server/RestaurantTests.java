c2f990df9fc6af449244c3bf6df3be35d5fe88ad
Shuqi <shuqihe95@gmail.com>
2018-04-22 14:48:05 -0400

    Finish RestaurantController unit test

--- src/test/java/com/picfood/server/RestaurantTests.java
+++ src/test/java/com/picfood/server/RestaurantTests.java
@@ -1,6 +1,8 @@
 package com.picfood.server;
 import java.util.*;
 import com.picfood.server.controller.RestaurantController;
+import com.picfood.server.entity.DTO.RestaurantDTO;
+import com.picfood.server.entity.DTO.RestaurantSearchDTO;
 import com.picfood.server.entity.Dish;
 import com.picfood.server.entity.Restaurant;
 import org.junit.Assert;
@@ -21,7 +23,7 @@ public class RestaurantTests {
 
     @Test
     public void testGetRestaurantInfo(){
-        Restaurant r1 = (Restaurant) restaurantController.getRestaurantInfo("basil-restaurant-new-haven-2");
+        RestaurantDTO r1 = (RestaurantDTO) restaurantController.getRestaurantInfo("basil-restaurant-new-haven-2");
         Assert.assertTrue(r1 != null || r1.getName().equals("Basil Restaurant"));
 
     }
@@ -41,4 +43,13 @@ public class RestaurantTests {
         List<Restaurant> result = restaurantController.getNearRestaurant(-72.9265664,41.3052498);
         Assert.assertTrue(result.size() > 0 || result.get(0).getRestaurantId().equals("cajun-boiled-seafood-ct-new-haven"));
     }
+    @Test
+    public void testSearchRestaurants(){
+        List<RestaurantSearchDTO> result = restaurantController.searchRestaurants("basil","distance", -72.92,41.30, 100.);
+        Assert.assertTrue(result.size() > 0 || result.get(0).getName().equals("Basil Restaurant"));
+        List<RestaurantSearchDTO> result1 = restaurantController.searchRestaurants("basil", "rate", -72.92,41.30, 100.);
+        Assert.assertTrue(result1.size() > 0 || result1.get(0).getName().equals("Basil Restaurant"));
+
+    }
+
 }
